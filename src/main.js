130bb36964fa5b94cce5ee8b020ac7811e6c6293
Luki0510 <luki888@qq.com>
2021-03-31 21:16:55 +1000

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import ZkTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应yangshi
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
+
+
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 
@@ -25,6 +34,21 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', ZkTable)
+    // 富文本的全局注册
+Vue.use(VueQuillEditor, /* { default global options } */ )
+
+// 全局时间处理过滤器
+Vue.filter('dataFormat', function(originVal) {
+    const dt = new Date(originVal)
+    const year = dt.getFullYear()
+    const month = (dt.getMonth() + 1 + '').padStart(2, '0')
+    const day = (dt.getDay() + '').padStart(2, '0')
+    const hh = (dt.getHours() + '').padStart(2, '0')
+    const min = (dt.getMinutes() + '').padStart(2, '0')
+    const sec = (dt.getSeconds() + '').padStart(2, '0')
+
+    return `${year}-${month}-${day} ${hh}:${min}:${sec}`
+})
 
 new Vue({
     router,
